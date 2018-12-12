e787b02ee6bd1566f78c50d87648b4dda0bf0368
Donovan McGillen <donovan.mcgillen@gmail.com>
2018-12-12 08:29:53 +0000

    Replace midje with clojure.test

--- test/leiningen/unit/dynamodb_local.clj
+++ test/leiningen/unit/dynamodb_local.clj
@@ -1,12 +1,9 @@
 (ns leiningen.unit.dynamodb-local
-  (:require [dynamodb-local.core :refer :all]
+  (:require [clojure.test :refer [deftest is testing]]
+            [dynamodb-local.core :as sut]
             [environ.core :refer [env]]
-            [midje
-             [sweet :refer :all]
-             [util :refer [testable-privates]]])
+            [medley.core :as medley])
   (:import [java.io File]))
-
-(testable-privates dynamodb-local.core build-dynamo-command)
 
 (def log-nothing (constantly nil))
 
@@ -19,46 +16,48 @@
   when building the java command to start DynamoDB Local."
   (str "-Djava.library.path=" dynamo-directory "/DynamoDBLocal_lib -jar " dynamo-directory "/DynamoDBLocal.jar"))
 
-(fact-group
- :unit
+(deftest build-dynamo-command
+  (testing "Command is built correctly from options"
+    (doseq [[dynamodb-local-config command] [[nil
+                                              (str "java  " dynamo-lib-paths " -port 8000 -dbPath " dynamo-directory)]
+                                             [{}
+                                              (str "java  " dynamo-lib-paths " -port 8000 -dbPath " dynamo-directory)]
+                                             [{:port "9999"}
+                                              (str "java  " dynamo-lib-paths " -port 9999 -dbPath " dynamo-directory)]
+                                             [{:in-memory? true}
+                                              (str "java  " dynamo-lib-paths " -port 8000 -inMemory")]
+                                             [{:in-memory? false}
+                                              (str "java  " dynamo-lib-paths " -port 8000 -dbPath " dynamo-directory)]
+                                             [{:db-path "some/path/that/will/be/ignored" :in-memory? true}
+                                              (str "java  " dynamo-lib-paths " -port 8000 -inMemory")]
+                                             [{:db-path "some/path"}
+                                              (str "java  " dynamo-lib-paths " -port 8000 -dbPath some/path")]
+                                             [{:shared-db? true}
+                                              (str "java  " dynamo-lib-paths " -port 8000 -sharedDb -dbPath " dynamo-directory)]
+                                             [{:shared-db? false}
+                                              (str "java  " dynamo-lib-paths " -port 8000 -dbPath " dynamo-directory)]
+                                             [{:jvm-opts ["opt1" "opt2"]}
+                                              (str "java opt1 opt2 " dynamo-lib-paths " -port 8000 -dbPath " dynamo-directory)]]]
+      (let [project  (-> {:some-key "some-val"}
+                         (medley/assoc-some :dynamodb-local dynamodb-local-config))]
+        (is (= (#'sut/build-dynamo-command log-nothing project)
+               command)))))
+  (testing "Port can be specified as an environment variable"
+    (with-redefs [env (constantly "7777")]
+      (doseq [[dynamodb-local-config port] [[{} "7777"]
+                                            [{:port "9999"} "9999"]]]
+        (let [project {:some-key "some-val"
+                       :dynamodb-local dynamodb-local-config}]
+          (is (= (#'sut/build-dynamo-command log-nothing project)
+                 (str "java  " dynamo-lib-paths " -port " port " -dbPath " dynamo-directory))))))))
 
- (tabular
-  (fact "Build dynamo command correctly builds command based on options set in the project map"
-        (let [project (merge {:some-key "some-val"}
-                             (when ?dynamodb-local-config
-                               {:dynamodb-local ?dynamodb-local-config}))]
-          (build-dynamo-command log-nothing project)) => ?command)
-  ?dynamodb-local-config                                       ?command
-  nil                                                          (str "java  " dynamo-lib-paths " -port 8000 -dbPath " dynamo-directory)
-  {}                                                           (str "java  " dynamo-lib-paths " -port 8000 -dbPath " dynamo-directory)
-  {:port "9999"}                                               (str "java  " dynamo-lib-paths " -port 9999 -dbPath " dynamo-directory)
-  {:in-memory? true}                                           (str "java  " dynamo-lib-paths " -port 8000 -inMemory")
-  {:in-memory? false}                                          (str "java  " dynamo-lib-paths " -port 8000 -dbPath " dynamo-directory)
-  {:db-path "some/path/that/will/be/ignored" :in-memory? true} (str "java  " dynamo-lib-paths " -port 8000 -inMemory")
-  {:db-path "some/path"}                                       (str "java  " dynamo-lib-paths " -port 8000 -dbPath some/path")
-  {:shared-db? true}                                           (str "java  " dynamo-lib-paths " -port 8000 -sharedDb -dbPath " dynamo-directory)
-  {:shared-db? false}                                          (str "java  " dynamo-lib-paths " -port 8000 -dbPath " dynamo-directory)
-  {:jvm-opts ["opt1" "opt2"]})                                 (str "java opt1 opt2 " dynamo-lib-paths " -port 8000 -dbPath " dynamo-directory)
-
-  (tabular
-   (fact "Build dynamo command allows port to be specified as an environment variable"
-         (let [project {:some-key "some-val"
-                        :dynamodb-local ?dynamodb-local-config}]
-           (build-dynamo-command log-nothing project) => (str "java  " dynamo-lib-paths " -port " ?port " -dbPath " dynamo-directory)
-           (provided
-            (env :dynamodb-port "8000") => "7777")))
-   ?dynamodb-local-config ?port
-   {}                     "7777"
-   {:port "9999"}         "9999")
-
-  (fact "Ensure installed does not download DynamoDB Local if it already has been"
-        (ensure-installed log-nothing) => nil
-        (provided
-          (#'dynamodb-local.core/exists? anything) => true))
-
-  (fact "Ensure installed downloads and unpacks DynamoDB Local if it hasn't been already"
-        (ensure-installed log-nothing) => ...unpack-result...
-        (provided
-          (#'dynamodb-local.core/exists? anything) => false
-          (#'dynamodb-local.core/download-dynamo log-nothing anything) => ...download-result...
-          (#'dynamodb-local.core/unpack-dynamo log-nothing) => ...unpack-result...)))
+(deftest ensure-installed
+  (testing "DynamoDB local is downloaded and unpacked if it hasn't been already"
+    (with-redefs [sut/exists? (constantly false)
+                  sut/download-dynamo (constantly :download-result)
+                  sut/unpack-dynamo (constantly :unpack-result)]
+      (is (= (sut/ensure-installed log-nothing)
+             :unpack-result))))
+  (testing "DynamoDB local not downloaded if it already has been"
+    (with-redefs [sut/exists? (constantly true)]
+      (is (nil? (sut/ensure-installed log-nothing))))))
