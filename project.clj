65dc1fd67eaebff5725202baa11e78499982ea97
Donovan McGillen <donovan.mcgillen@gmail.com>
2018-12-12 07:53:15 +0000

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-dynamodb-local "0.1.2-SNAPSHOT"
+(defproject clj-dynamodb-local "0.1.2"
   :description "A Leiningen plugin and boot task for providing a local DynamoDB instance"
   :url "https://github.com/dmcgillen/clj-dynamodb-local"
   :license {:name "Eclipse Public License"
